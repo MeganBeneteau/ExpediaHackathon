2e95ec96e0cf4964bbe726665d8746ca27690408
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-05-22 21:29:51 -0400

    Add VERY basic api calls and design

--- www/js/index.js
+++ www/js/index.js
@@ -16,35 +16,80 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+// var app = {
+//     // Application Constructor
+//     initialize: function() {
+//         this.bindEvents();
+//     },
+//     // Bind Event Listeners
+//     //
+//     // Bind any events that are required on startup. Common events are:
+//     // 'load', 'deviceready', 'offline', and 'online'.
+//     bindEvents: function() {
+//         document.addEventListener('deviceready', this.onDeviceReady, false);
+//     },
+//     // deviceready Event Handler
+//     //
+//     // The scope of 'this' is the event. In order to call the 'receivedEvent'
+//     // function, we must explicitly call 'app.receivedEvent(...);'
+//     onDeviceReady: function() {
+//         app.receivedEvent('deviceready');
+//     },
+//     // Update DOM on a Received Event
+//     receivedEvent: function(id) {
+//         var parentElement = document.getElementById(id);
+//         var listeningElement = parentElement.querySelector('.listening');
+//         var receivedElement = parentElement.querySelector('.received');
+
+//         listeningElement.setAttribute('style', 'display:none;');
+//         receivedElement.setAttribute('style', 'display:block;');
+
+//         console.log('Received Event: ' + id);
+//     }
+// };
+
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    posts_url: "http://terminal2.expedia.com/x/activities/search?location=London&startDate=2015-08-08&endDate=2015-08-18&apikey=<API KEY>",
+
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        console.log('Device is ready');
+        app.readPosts();
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    readPosts: function() {
+        console.log('Reading posts');
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: app.posts_url,
+            success: app.onSuccess,
+            error: app.onError
+        });
 
-        console.log('Received Event: ' + id);
+        console.log('Reading posts asynchrounously');
+    },
+
+    onSuccess: function(data) {
+        var items = [];
+        $.each(data.activities, function(key, val){
+            items.push('<a href="' + app.posts_url + val.id + '">' + val.id + ' - ' +val.title + ' <img src="'+val.imageUrl+'" /></a>');
+        });
+        $('#posts').html(items.join('<br/>'));
+        console.log('Exiting onSuccess');
+    },
+
+    onError: function(data, textStatus, errorThrown) {
+        console.log('Data: ' + data);
+        console.log('Status: ' + textStatus);
+        console.log('Error: ' + errorThrown);
+        $("#posts").html('Error while loading posts');
+        console.log('Exiting onError');
     }
 };
-app.initialize();
+
+$(document).ready(function() { 
+    // jQuery is properly loaded at this point
+    // so proceed to bind the Cordova's deviceready event
+    //app.readPosts();
+    $(document).bind('deviceready', app.onDeviceReady); 
+});
